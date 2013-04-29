bc7e2a0fb9e7ad7e986a933dc39f2defe4122fb5
George Courtsunis <gjcourt@gmail.com>
2013-04-29 12:03:13 -0700

    Add sample test for hits/attempts

---
+++ bandit/example/simple.py
@@ -0,0 +1,61 @@
+from bandit import test, logger, backend
+from bandit.choice import choice
+
+
+class MyLogger(logger.Logger):
+    """
+    Example logger
+    """
+    def key(self, name, id):
+        return name + ' ' + str(id)
+
+    def hit(self, choice, **kwargs):
+        print 'Logging Hit', choice, kwargs
+        key = self.key(kwargs['meta']['name'], choice.id)
+        self.hits[key] += 1
+
+    def attempt(self, choice, **kwargs):
+        print 'Logging Attempt', choice, kwargs
+        key = self.key(kwargs['meta']['name'], choice.id)
+        self.attempts[key] += 1
+
+
+class MyBackend(backend.BanditBackend):
+    """
+    Example backend
+    """
+    def select(self, limit=None, **kwargs):
+        return self.choices[:limit]
+
+
+class MyTest(test.Test):
+    """
+    Example test
+    """
+    logger = MyLogger
+    backend = MyBackend
+
+
+class MyOtherTest(test.Test):
+    logger = MyLogger
+    backend = MyBackend
+
+
+# Create a few tests
+test = MyTest([choice(i,i) for i in range(3)])
+test2 = MyOtherTest([choice(i,i) for i in range(3)])
+
+# Some test metadata
+meta = {'name': 'foo'}
+
+# Select choices (attempts implicitly logged)
+test_choices = test.select(2, meta=meta)
+test2_choices = test2.select(3, meta=meta)
+
+# Log hits
+test.hit(test_choices[0], meta=meta)
+test2.hit(test2_choices[0], meta=meta)
+
+print test.data()
+print test2.data()
+
